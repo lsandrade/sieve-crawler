7e5c75d51348744dc1842753893316590cca37f2
Luan Andrade <luansandrade92@hotmail.com>
2016-07-05 17:38:48 -0300

    test valid urls

--- crawler.py
+++ crawler.py
@@ -5,12 +5,36 @@ import requests
 
 class Crawler():
 
-    def ___init___(self):
-        print("Hi")
+    urls = []
 
+    # set main URL
     def setUrl(self,url):
         self.url = url
 
+    # get main URL
     def getUrl(self):
         return self.url
 
+    # save URL visited in an array
+    # return index of array
+    def keepUrl(self,url):
+        self.urls.append(url)
+        return len(self.urls)-1
+
+    # return array of visited URLs
+    def getUrlsVisited(self):
+        return self.urls
+
+    # get an URL visited from array according an index
+    def getUrlsVisitedByIndex(self,i):
+        return self.urls[i]
+
+    # verify if is a valid URL.
+    # criteria: starts with the main URL and it was not visited preveously
+    def isValidUrl(self,url):
+        if url[:33]==self.url and url not in self.urls:
+            return True
+        else:
+            return False
+
+    
--- testeunitario.py
+++ testeunitario.py
@@ -6,21 +6,58 @@ from crawler import Crawler
 
 class CrawlerTest(unittest.TestCase):
 
-	# out crawler to be tested
+	# our crawler to be tested
 	crawler = Crawler()
+	url = "http://www.epocacosmeticos.com.br"
 
 	# test if crawler was created
 	def testCrawler(self):
 		self.assertIsInstance(self.crawler,Crawler,"Not a Crawler.")
+		
 
 	# test if URL is OK
 	def testUrl(self):
-		url = "http://www.epocacosmeticos.com.br"
-		self.crawler.setUrl(url)
-		self.assertEqual(self.crawler.getUrl(),url, "Wrong URL.")
+		self.crawler.setUrl(self.url)
+		index = self.crawler.keepUrl(self.url)
 
-		self.assertEqual(requests.get(url).status_code,200, "URL can't be accessed.")
+		self.assertEqual(self.crawler.getUrl(),self.url, "Wrong URL.")
+		self.assertNotEqual(self.crawler.getUrl(), "http://www.google.com", "It's weird!")
+		self.assertEqual(requests.get(self.url).status_code,200, "URL can't be accessed.")
+		self.assertEqual(self.crawler.getUrlsVisitedByIndex(index),self.url,"Url visited is not the Url saved.")
 
+	# Test if our crawler can keep an array of URLs visited
+	def testArrayofUrls(self):
+		urls = ["http://www.google.com/2","http://www.facebook.com"]
+		
+		self.crawler.keepUrl(urls[0])
+		self.crawler.keepUrl(urls[1])
+		
+		self.assertEqual(self.crawler.getUrlsVisited(),urls,"Array of URLs is wrong.")
+		self.assertEqual(self.crawler.getUrlsVisitedByIndex(1),urls[1],"URL saved is wrong")
+
+	# Verify if the URL is valid
+	def testValidUrl(self):
+		url1 = "http://www.google.com/1"
+		url2 = "#"
+		url3 = "http://www.epocacosmeticos.com.br/inner-restore-intensif-senscience-mascara-reconstrutora/p"
+
+		self.assertFalse(self.crawler.isValidUrl(url1), "Not a valid URL.")
+		self.assertFalse(self.crawler.isValidUrl(url2), "Not a valid URL.")
+		self.assertFalse(self.crawler.isValidUrl(self.url), "Not a valid URL.")
+		self.assertTrue(self.crawler.isValidUrl(url3), "Not a valid URL.")
+	# Verify if a page is Product page or Normal page
+
+
+	# Get all links from page visited
+
+
+	# create CSV to store data
+
+
+	# Verify if a product was saved in the store data
+
+
+	# Test time of execution with multithread
 
 if __name__ == '__main__':
     unittest.main()
