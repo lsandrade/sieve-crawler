1ab51bec3b16fbca34863606867c80d39fd164a2
Luan Andrade <luansandrade92@hotmail.com>
2016-07-05 16:09:09 -0300

    test url ok

--- testeunitario.py
+++ testeunitario.py
@@ -5,10 +5,18 @@ from crawler import Crawler
 
 class CrawlerTest(unittest.TestCase):
 
+	# out crawler to be tested
 	crawler = Crawler()
 
+	# test if crawler was created
 	def testCrawler(self):
 		self.assertIsInstance(self.crawler,Crawler,"Not a Crawler.")
+
+	# test if URL is OK
+	def testUrl(self):
+		url = "http://www.epocacosmeticos.com.br"
+		self.crawler.setUrl(url)
+		self.assertEqual(self.crawler.getUrl(),url, "Wrong URL.")
 
 
 if __name__ == '__main__':
