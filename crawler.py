11220d246f8311ac9b92d241b1fdceaeabdd5be0
Luan Andrade <luansandrade92@hotmail.com>
2016-07-05 19:58:46 -0300

    test get All Links, create Csv, get Product Data

--- crawler.py
+++ crawler.py
@@ -1,7 +1,10 @@
-
+# -*- coding: utf-8 -*-
 
 import requests
-
+from bs4 import BeautifulSoup
+import csv
+import urllib.request
+import sys
 
 class Crawler():
 
@@ -44,3 +47,56 @@ class Crawler():
             return True
         else:
             return False
+
+    # Getting all links from a page
+    def getAllLinks(self,url):
+        links = []
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text,"html.parser")
+        for a in soup.findAll('a',href=True):
+            links.append(a['href'])
+        return links
+
+    # creating objects to read and write csv file
+    def createCsv(self,title):
+        writer = open(title, 'w',encoding='utf-8')
+        self.title = title
+
+        writer.write('ref,name,price,availability,description,url\n')
+        writer.close()
+
+        
+    # getting csv title
+    def getCsvTitle(self):
+        return self.title
+
+    # getting product data from URL
+    def getProduct(self,url):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text,"html.parser")
+        ref = soup.find('label',class_='sku-ean-code').string
+        name = soup.find('div',class_='productName').string
+        try:
+            price = soup.find('strong',class_='skuBestPrice').string
+            availability = "Yes"
+        except AttributeError:
+            price = "*"
+            availability = "No"
+        description = soup.find('div',class_='productDescription').text
+        
+        product = {'ref':ref, 'name':name, 'price':price, 'availability':availability, 'description': description, 'url':url}
+        
+        return product
+
+    #store product in CSV File
+    def storeProduct(self,product):
+        writer = open(self.title, 'a', encoding='utf-8')
+        writer.write(product['ref']+","+product['name']+","+product['price']+","+product['availability']+","+product['description']+","+product['url']+"\n")
+        writer.close()
+
+
+    def getStoredProductByIndex(self,i):
+        reader = open(self.title,'r',encoding='utf-8')
+        lines = reader.read().splitlines()
+        reader.close()
+        return lines[i+1].split(',')
--- testeunitario.py
+++ testeunitario.py
@@ -49,17 +49,55 @@ class CrawlerTest(unittest.TestCase):
 	# Check if a page is Product page or Normal page
 	def testUrlProduct(self):
 		url3 = "http://www.epocacosmeticos.com.br/inner-restore-intensif-senscience-mascara-reconstrutora/p"
+		
 		self.assertTrue(self.crawler.isProduct(url3),"I'ts not a product.")
 		self.assertFalse(self.crawler.isProduct(self.url),"I'ts not a product.")
 		self.assertFalse(self.crawler.isProduct("http://www.epocacosmeticos.com.br/cabelos"),"I'ts not a product.")
 
 	# Get all links from page visited
+	def testGetAllLinks(self):
+		simple_page = "http://mussumipsum.com/"
+
+		self.assertEqual(len(self.crawler.getAllLinks(simple_page)),7,"List of links muts contain 7 links.")
 
 
 	# create CSV to store data
+	def testCreateCsvFile(self):
+		title = "file.csv"
+
+		self.crawler.createCsv(title)
+		
+		self.assertEqual(self.crawler.getCsvTitle(),title,"Csv doesn't exists")
+
+	# get product data from URL
+	def testProductData(self):
+		url3 = "http://www.epocacosmeticos.com.br/inner-restore-intensif-senscience-mascara-reconstrutora/p"
+		url4 = "http://www.epocacosmeticos.com.br/vip-old-spice-perfume-para-o-corpo/p"
+
+		#product available
+		product = self.crawler.getProduct(url3)
+
+		self.assertEqual(product['name'],"Inner Restore Intensif Senscience - Máscara Reconstrutora","Name wrong")
+		self.assertEqual(product['ref'],"0074469445320","Ref wrong")
+		self.assertEqual(product['price'],"R$ 62,40","Price wrong")
+		#self.assertEqual(product['description'],"","Name wrong")
+		self.assertEqual(product['availability'],"Yes","Availability wrong")
+
+		#product unavailable
+		product = self.crawler.getProduct(url4)
+		self.assertEqual(product['price'],"*","Price wrong")
+		self.assertEqual(product['availability'],"No","Availability wrong")
 
 
 	# Verify if a product was saved in the store data
+	def testStoreProduct(self):
+		url3 = "http://www.epocacosmeticos.com.br/inner-restore-intensif-senscience-mascara-reconstrutora/p"
+		
+		product = self.crawler.getProduct(url3)
+
+		self.crawler.storeProduct(product)
+
+		self.assertEqual(self.crawler.getStoredProductByIndex(0)[0],product['ref'],"Product not sotred.")
 
 
 	# Test time of execution with multithread
